ea4dad2007e77122fd868055fa5520d4ff44f1a0
ilnar <ilnar-sov@mail.ru>
2022-08-06 14:48:09 +0300

    Задача 3 решена

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,3 +1,20 @@
 ﻿// Найти максимальное из трех чисел
 Console.Clear();
 
+int number1 = new Random().Next(0,500);
+int number2 = new Random().Next(0,500);
+int number3 = new Random().Next(0,500);
+
+int max = number1;
+if (number2 > number1)
+{
+    if (number2 > number3)
+    {
+        max = number2;
+    }
+    else {max = number3;}
+}    
+
+else if (number3 > number1) {max = number3;}
+
+Console.WriteLine("Максимальное из трёх числел: " +number1+ ", " +number2+ ", " +number3+ " - будет " +max);
