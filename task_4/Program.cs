6dedd65318a40f619a00798a36f7b13462ce239a
ilnar <ilnar-sov@mail.ru>
2022-08-06 14:59:45 +0300

    Задача 4 решена

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -1,3 +1,8 @@
 ﻿// Выяснить является ли число чётным
 Console.Clear();
 
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) {Console.Write("Число " +number+ " является чётным");}
+else {Console.Write("Число " +number+ " не является чётным");}
