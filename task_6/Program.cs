96f2b7f3e385e1f87bdbf6b2cdc033c14526d206
ilnar <ilnar-sov@mail.ru>
2022-08-06 17:12:41 +0300

    Задача 6 решена

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -1,3 +1,8 @@
 ﻿// Показать последнюю цифру трёхзначного числа
 Console.Clear();
 
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = number % 10;
+Console.Write("Последняя цифра числа " +number+ ": " + Math.Abs(result));
