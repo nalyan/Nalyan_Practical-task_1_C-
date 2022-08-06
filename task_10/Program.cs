0552e57d4890fa3150b08d4a27d9e3adf7392ff2
ilnar <ilnar-sov@mail.ru>
2022-08-06 20:42:06 +0300

    Задача 10 решена

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,2 +1,11 @@
 ﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
 Console.Clear();
+
+Console.Write("Введите число, проверяемое на кратность: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int number2 = int.Parse(Console.ReadLine());
+int remains = number1 % number2;
+
+if (number1 % number2 ==0) {Console.Write("Число " +number1+ " кратно числу " +number2);}
+else {Console.Write("Остаток от деления числа " +number1+ " на число " +number2+ " равен " +remains);}
