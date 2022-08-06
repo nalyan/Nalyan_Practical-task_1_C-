edd000dba35b952b16ee0118e88a530eb8ab1713
ilnar <ilnar-sov@mail.ru>
2022-08-06 15:10:53 +0300

    Задача 5 решена

--- task_5/Program.cs
+++ task_5/Program.cs
@@ -1,4 +1,11 @@
 ﻿// Показать числа от -N до N
 Console.Clear();
 
-
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int count = -n;
+while (count <= n)
+{
+    Console.Write(count+ " ");
+    count++;
+}
