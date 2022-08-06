3975ec946cf3fc7f6b5091e2d1e05d80e57cc7a3
ilnar <ilnar-sov@mail.ru>
2022-08-06 18:10:01 +0300

    Задача 8 решена

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -1,2 +1,9 @@
 ﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
 Console.Clear();
+
+int number = new Random().Next(10,100);
+int count1 = (number % 100) / 10;
+int count2 = number % 10;
+
+if (count1 > count2) {Console.Write("Максимальная цифра числа " +number+ ": " +count1);}
+else {Console.Write("Максимальная цифра числа " +number+ ": " +count2);}
