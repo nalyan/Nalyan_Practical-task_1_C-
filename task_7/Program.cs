638687c487a9e4d9192c681c8b7c8df92925d919
ilnar <ilnar-sov@mail.ru>
2022-08-06 17:54:14 +0300

    Задача 7 решена

--- task_7/Program.cs
+++ task_7/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Показать вторую цифру трёхзначного числа
 Console.Clear();
 
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number / 100 < 1 || number / 1000 >= 1) {Console.Write("Это не трёхзначное число");}
+else
+{
+int remains100 = number % 100;
+int result = remains100 / 10;
 
+Console.Write("Вторая цифра числа " + number + ": "+  Math.Abs(result));
+}
