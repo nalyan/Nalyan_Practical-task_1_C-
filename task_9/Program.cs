8cbc5a533eb3783cc52b2742dbca96556e81d675
ilnar <ilnar-sov@mail.ru>
2022-08-06 20:22:23 +0300

    Задача 9 решена

--- task_9/Program.cs
+++ task_9/Program.cs
@@ -1,2 +1,9 @@
 ﻿// Удалить вторую цифру трёхзначного числа
 Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int figure2 = (number % 100) / 10;
+int figure3 = number % 10;
+int result = number / 10 - figure2 + figure3;
+Console.Write("Результат удаления второй цифры из числа " +number+ " равен: " +result);
