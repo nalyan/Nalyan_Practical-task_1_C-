79dddcbf716f44a8c5f9886b6971a25ded17e902
ilnar <ilnar-sov@mail.ru>
2022-08-06 23:26:46 +0300

    Задача 11 решена

--- task_11/Program.cs
+++ task_11/Program.cs
@@ -1,3 +1,27 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
 Console.Clear();
 
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 100;
+int multi = 1;
+if (Math.Abs(number) / count < 1) { Console.Write("В этом числе нет третьей цифры"); }
+else
+{
+    while (multi == 1)
+    {
+        if (Math.Abs(number) / count < 1) { multi = count / 100; }
+        else { count = count * 10; }
+    }
+    int result = number % multi;
+    if (multi == 10) { Console.WriteLine("Третья цифра в числе " + number + ": " + Math.Abs(result)); }
+    else
+    {
+        while (multi != 10)
+        {
+            result = result / 10;
+            multi = multi / 10;
+        }
+    Console.WriteLine("Третья цифра в числе " + number + ": " + Math.Abs(result));
+    }
+}
